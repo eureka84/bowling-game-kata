2fb6e71246ee4b129ee622e7524166fe241792b6
Angelo Sciarra <asciarra@byte-code.com>
2017-11-12 18:16:17 +0100

    I can handle the case of 2 non consecutive strikes

--- src/test/java/com/eureka/katas/bowling/BowlingGameSpec.kt
+++ src/test/java/com/eureka/katas/bowling/BowlingGameSpec.kt
@@ -11,54 +11,81 @@ class BowlingGameSpec: StringSpec() {
 
         "should be able to tell the score after no roll" {
             val game = Game()
+
             game.score() shouldBe 0
         }
 
         "should be able to tell the score after one roll" {
             val game = Game()
+
             game.roll(4)
+
             game.score() shouldBe 4
         }
 
         "should be able to tell the score after two rolls with less than 10 pins down" {
             val game = Game()
+
             game.roll(3)
             game.roll(4)
+
             game.score() shouldBe 7
         }
 
         "should be able to tell the score after one spare" {
             val game = Game()
+
             rollOneSpare(game)
             game.roll(3)
+
             game.score() shouldBe 16
         }
 
         "should be able to tell the score after two spares" {
             val game = Game()
+
             rollOneSpare(game)
             game.roll(3)
             game.roll(4)
             rollOneSpare(game)
             game.roll(4)
+
             game.score() shouldBe 38
         }
 
         "should be able to tell the score after two consecutive spares" {
             val game = Game()
+
             rollOneSpare(game)
             rollOneSpare(game)
             game.roll(3)
+
             game.score() shouldBe 30
         }
 
         "should be able to tell the score after one strike" {
             val game = Game()
+
             rollAStrike(game)
             game.roll(3)
             game.roll(4)
+
             game.score() shouldBe 24
         }
+
+        "should be able to tell the score after two non consecutives strikes" {
+            val game = Game()
+
+            rollAStrike(game)
+            game.roll(3)
+            game.roll(4)
+            rollAStrike(game)
+            game.roll(4)
+            game.roll(1)
+
+            game.score() shouldBe 44
+        }
+
 
     }
 
