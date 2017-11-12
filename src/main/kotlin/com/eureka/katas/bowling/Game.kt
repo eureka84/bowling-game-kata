dc253d726e971e13b29e271f22171ff579c7bd70
Angelo Sciarra <asciarra@byte-code.com>
2017-11-12 16:28:55 +0100

    extracted some methods to enhance clarity

--- src/main/kotlin/com/eureka/katas/bowling/Game.kt
+++ src/main/kotlin/com/eureka/katas/bowling/Game.kt
@@ -8,12 +8,21 @@ class Game {
         var total = 0
         rolls.forEachIndexed { index, pinsDown ->
             total += pinsDown
-            if (index > 1 && index % 2 == 0 && (rolls[index - 1] + rolls[index - 2] == 10)) {
+            if (isNotFirstFrame(index) &&
+                isFirstThrowOfNextFrame(index) &&
+                previousFrameIsSpare(index)
+            ) {
                 total += pinsDown
             }
         }
         return total
     }
+
+    private fun previousFrameIsSpare(index: Int) = rolls[index - 1] + rolls[index - 2] == 10
+
+    private fun isFirstThrowOfNextFrame(index: Int) = index % 2 == 0
+
+    private fun isNotFirstFrame(index: Int) = index > 1
 
     fun roll(pinsDown: Int) {
         rolls.add(pinsDown)
