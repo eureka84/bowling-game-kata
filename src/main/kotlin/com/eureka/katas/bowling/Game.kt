4502b34a3b7d6177127e1cfc16e70bba887d910a
asciarra <angelo.sciarra@lastminute.com>
2017-11-13 11:32:02 +0100

    some code rearrangement

--- src/main/kotlin/com/eureka/katas/bowling/Game.kt
+++ src/main/kotlin/com/eureka/katas/bowling/Game.kt
@@ -34,20 +34,6 @@ class Game {
         return this.zip(other).map { (a, b) -> a + b }
     }
 
-    private fun pointsExtraForSpare(): List<Int> {
-        return (1..listOfPinsDown.size).map { currentRoll ->
-            if (isThereASparePending(currentRoll))
-                listOfPinsDown[indexOf(currentRoll)]
-            else 0
-        }
-    }
-
-    private fun pointsExtraForStrike(): List<Int> {
-        return (1..listOfPinsDown.size).map { currentRoll ->
-            listOfPinsDown[indexOf(currentRoll)] * numberOfStrikesPending(currentRoll)
-        }
-    }
-
     private fun basePoints(): List<Int> {
         return (1..listOfPinsDown.size).map { currentRoll ->
             if (isNotBonusThrow(currentRoll))
@@ -56,27 +42,38 @@ class Game {
         }
     }
 
-    private fun numberOfStrikesPending(roll: Int) =
-            (Math.max(1, roll - 2) until roll)
-                    .filter(this::isStrike)
-                    .count()
-
-    private fun isStrike(roll: Int) =
-            isNotBonusThrow(roll) && listOfPinsDown[indexOf(roll)] == 10
-
     private fun isNotBonusThrow(roll: Int) = frames[roll]!! <= 10
+
+    private fun indexOf(roll: Int) = roll - 1
+
+    private fun pointsExtraForSpare(): List<Int> {
+        return (1..listOfPinsDown.size).map { currentRoll ->
+            if (isThereASparePending(currentRoll))
+                listOfPinsDown[indexOf(currentRoll)]
+            else 0
+        }
+    }
 
     private fun isThereASparePending(roll: Int) =
             isFramesFirstThrow(roll) && previousFrameIsSpare(roll) && frames[roll]!! <= 11
+
+    private fun isFramesFirstThrow(roll: Int) = roll % 2 == 1
 
     private fun previousFrameIsSpare(roll: Int) =
             pinsDownFor(roll - 1) + pinsDownFor(roll - 2) == 10
 
     private fun pinsDownFor(roll: Int) = if (roll >= 1) listOfPinsDown[indexOf(roll)] else 0
 
-    private fun isFramesFirstThrow(roll: Int) = roll % 2 == 1
+    private fun pointsExtraForStrike(): List<Int> {
+        return (1..listOfPinsDown.size).map { currentRoll ->
+            listOfPinsDown[indexOf(currentRoll)] * numberOfStrikesPending(currentRoll)
+        }
+    }
 
-    private fun indexOf(roll: Int) = roll - 1
+    private fun numberOfStrikesPending(roll: Int) =
+            (Math.max(1, roll - 2) until roll)
+                    .filter{ roll -> isNotBonusThrow(roll) && listOfPinsDown[indexOf(roll)] == 10}
+                    .count()
 
     fun roll(pinsDown: Int) {
         listOfPinsDown.add(pinsDown)
