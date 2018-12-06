77296d3de751561183120146fe43d5b8f87f0bc6
eureka84 <angelosciarra@ymail.com>
2018-12-06 08:20:24 +0100

    refactor: introduced new typealias

--- src/main/kotlin/com/eureka/katas/bowling/Frame.kt
+++
@@ -1,6 +0,0 @@
-package com.eureka.katas.bowling
-
-typealias Frame = List<Int>
-
-fun Frame.isStrike(): Boolean = this.sum() == TOTAL_PINS && this.size == 1
-fun Frame.isSpare(): Boolean = this.sum() == TOTAL_PINS && this.size == MAX_THROWS_PER_FRAME
--- src/main/kotlin/com/eureka/katas/bowling/Game.kt
+++ src/main/kotlin/com/eureka/katas/bowling/Game.kt
@@ -4,12 +4,15 @@ const val TOTAL_PINS = 10
 const val MAX_THROWS_PER_FRAME = 2
 const val NUMBER_OF_FRAMES = 10
 
+typealias PinsKnockedDown = Int
+typealias Frame = List<Int>
+
 class Game {
 
-    private val rolls = mutableListOf<Int>()
+    private val rolls = mutableListOf<PinsKnockedDown>()
 
-    fun roll(pinsKnockedDown: Int) {
-        rolls.add(pinsKnockedDown)
+    fun roll(p: PinsKnockedDown) {
+        rolls.add(p)
     }
 
     fun score(): Int {
@@ -33,21 +36,24 @@ class Game {
     }
 
     private fun computeFrames(): List<Frame> {
-        fun loop(rolls: List<Int>, frame: Frame, frames: List<Frame>, throws: Int, knockedDown: Int): List<Frame> {
-            val isFrameCompleted = throws == MAX_THROWS_PER_FRAME || knockedDown == TOTAL_PINS
+        fun loop(rolls: List<PinsKnockedDown>, frame: Frame, frames: List<Frame>, throws: Int, k: PinsKnockedDown): List<Frame> {
+            val isFrameCompleted = throws == MAX_THROWS_PER_FRAME || k == TOTAL_PINS
             return when {
                 rolls.isEmpty() ->
                     if (frame.isEmpty()) frames else frames.plus<Frame>(frame)
                 isFrameCompleted ->
                     loop(rolls, listOf(), frames.plus<Frame>(frame), 0, 0)
                 else ->
-                    loop(rolls.tail(), frame.plus(rolls.head()), frames, throws + 1, knockedDown + rolls.head())
+                    loop(rolls.tail(), frame.plus(rolls.head()), frames, throws + 1, k + rolls.head())
             }
         }
         return loop(rolls, listOf(), listOf(), 0, 0)
     }
 
 }
+
+fun Frame.isStrike(): Boolean = this.sum() == TOTAL_PINS && this.size == 1
+fun Frame.isSpare(): Boolean = this.sum() == TOTAL_PINS && this.size == MAX_THROWS_PER_FRAME
 
 private fun <T> List<T>.tail(): List<T> =
         if (this.isEmpty()) throw IllegalAccessException("Tail of empty list") else this.subList(1, this.size)
