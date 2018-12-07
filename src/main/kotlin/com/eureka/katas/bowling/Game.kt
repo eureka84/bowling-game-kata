1b1df8a1de432d587e26a29f499d0d72c7eccbbd
eureka84 <angelosciarra@ymail.com>
2018-12-07 22:02:14 +0100

    refactor: extracted a couple of functions from score function to improve readability

--- src/main/kotlin/com/eureka/katas/bowling/Game.kt
+++ src/main/kotlin/com/eureka/katas/bowling/Game.kt
@@ -16,26 +16,18 @@ class Game {
     }
 
     fun score(): Int {
-        val frames = computeFrames()
-        val nextTwoThrowsPinsKnockedDown = { i: Int ->
-            when {
-                frames[i + 1].isStrike() -> TOTAL_PINS + frames[i + 2][0]
-                else -> frames[i + 1].sum()
-            }
-        }
-        val isBonusFrame = { frameNumber: Int -> frameNumber > NUMBER_OF_FRAMES - 1 }
-
+        val frames = currentFrames()
         return frames.mapIndexed { frameNumber, frame ->
             when {
                 isBonusFrame(frameNumber) -> 0
-                frame.isSpare() -> TOTAL_PINS + frames[frameNumber + 1][0]
-                frame.isStrike() -> TOTAL_PINS + nextTwoThrowsPinsKnockedDown(frameNumber)
+                frame.isSpare() -> TOTAL_PINS + frames[frameNumber + 1].firstThrow()
+                frame.isStrike() -> TOTAL_PINS + nextTwoThrowsPinsKnockedDown(frames, frameNumber)
                 else -> frame.sum()
             }
         }.sum()
     }
 
-    private fun computeFrames(): List<Frame> {
+    private fun currentFrames(): List<Frame> {
         val isFrameCompleted = { throws: Int, k: PinsKnockedDown -> throws == MAX_THROWS_PER_FRAME || k == TOTAL_PINS }
         fun loop(ps: List<PinsKnockedDown>, f: Frame, fs: List<Frame>, throws: Int, k: PinsKnockedDown): List<Frame> {
             return when {
@@ -47,10 +39,19 @@ class Game {
         return loop(rolls, listOf(), listOf(), 0, 0)
     }
 
+    private fun nextTwoThrowsPinsKnockedDown(frames: List<Frame>, frameNumber: Int) =
+        when {
+            frames[frameNumber + 1].isStrike() -> TOTAL_PINS + frames[frameNumber + 2].firstThrow()
+            else -> frames[frameNumber + 1].sum()
+        }
+
+    private fun isBonusFrame(frameNumber: Int) = frameNumber > NUMBER_OF_FRAMES - 1
+
 }
 
 fun Frame.isStrike(): Boolean = this.sum() == TOTAL_PINS && this.size == 1
 fun Frame.isSpare(): Boolean = this.sum() == TOTAL_PINS && this.size == MAX_THROWS_PER_FRAME
+fun Frame.firstThrow(): PinsKnockedDown = this[0]
 
 private fun <T> List<T>.tail(): List<T> =
         if (this.isEmpty()) throw IllegalAccessException("Tail of empty list") else this.subList(1, this.size)
