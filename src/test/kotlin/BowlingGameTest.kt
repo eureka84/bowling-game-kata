d3599753a624a97c97315c289c05be1a623a2ac6
eureka84 <angelosciarra@ymail.com>
2018-12-10 22:52:05 +0100

    minor refactoring: rewrote as a Junit test

--- src/test/kotlin/BowlingGameTest.kt
+++ src/test/kotlin/BowlingGameTest.kt
@@ -1,111 +1,118 @@
 package com.eureka.katas.bowling
 
 import io.kotlintest.matchers.shouldBe
-import io.kotlintest.specs.StringSpec
+import org.junit.Test
 
-class BowlingGameTest : StringSpec() {
+class BowlingGameTest {
 
-    init {
+    @Test
+    fun `should be able to tell the score after no roll`() {
+        val game = Game()
 
-        "should be able to tell the score after no roll" {
-            val game = Game()
+        game.score() shouldBe 0
+    }
 
-            game.score() shouldBe 0
-        }
+    @Test
+    fun `should be able to tell the score after one roll`() {
+        val game = Game()
 
-        "should be able to tell the score after one roll" {
-            val game = Game()
+        game.roll(4)
 
-            game.roll(4)
+        game.score() shouldBe 4
+    }
 
-            game.score() shouldBe 4
-        }
+    @Test
+    fun `should be able to tell the score after two rolls with less than 10 pins down`() {
+        val game = Game()
 
-        "should be able to tell the score after two rolls with less than 10 pins down" {
-            val game = Game()
+        game.roll(3)
+        game.roll(4)
 
-            game.roll(3)
-            game.roll(4)
+        game.score() shouldBe 7
+    }
 
-            game.score() shouldBe 7
-        }
+    @Test
+    fun `should be able to tell the score after one spare`() {
+        val game = Game()
 
-        "should be able to tell the score after one spare" {
-            val game = Game()
+        rollOneSpare(game)
+        game.roll(3)
 
-            rollOneSpare(game)
-            game.roll(3)
+        game.score() shouldBe 16
+    }
 
-            game.score() shouldBe 16
-        }
+    @Test
+    fun `should be able to tell the score after two spares`() {
+        val game = Game()
 
-        "should be able to tell the score after two spares" {
-            val game = Game()
+        rollOneSpare(game)
+        game.roll(3)
+        game.roll(4)
+        rollOneSpare(game)
+        game.roll(4)
 
-            rollOneSpare(game)
-            game.roll(3)
-            game.roll(4)
-            rollOneSpare(game)
-            game.roll(4)
+        game.score() shouldBe 38
+    }
 
-            game.score() shouldBe 38
-        }
+    @Test
+    fun `should be able to tell the score after two consecutive spares`() {
+        val game = Game()
 
-        "should be able to tell the score after two consecutive spares" {
-            val game = Game()
+        rollOneSpare(game)
+        rollOneSpare(game)
+        game.roll(3)
 
-            rollOneSpare(game)
-            rollOneSpare(game)
-            game.roll(3)
+        game.score() shouldBe 30
+    }
 
-            game.score() shouldBe 30
-        }
+    @Test
+    fun `should be able to tell the score after one strike`() {
+        val game = Game()
 
-        "should be able to tell the score after one strike" {
-            val game = Game()
+        rollAStrike(game)
+        game.roll(3)
+        game.roll(4)
 
-            rollAStrike(game)
-            game.roll(3)
-            game.roll(4)
+        game.score() shouldBe 24
+    }
 
-            game.score() shouldBe 24
-        }
+    @Test
+    fun `should be able to tell the score after two non consecutive strikes`() {
+        val game = Game()
 
-        "should be able to tell the score after two non consecutive strikes" {
-            val game = Game()
+        rollAStrike(game) // -> 17
+        game.roll(3)
+        game.roll(4)
+        rollAStrike(game) // -> 15
+        game.roll(4)
+        game.roll(1)
 
-            rollAStrike(game) // -> 17
-            game.roll(3)
-            game.roll(4)
-            rollAStrike(game) // -> 15
-            game.roll(4)
-            game.roll(1)
+        game.score() shouldBe 44
+    }
 
-            game.score() shouldBe 44
-        }
+    @Test
+    fun `should be able to tell the score after two consecutive strikes`() {
+        val game = Game()
 
-        "should be able to tell the score after two consecutive strikes" {
-            val game = Game()
+        rollAStrike(game) // -> 24
+        rollAStrike(game) // -> 15
+        game.roll(4)
+        game.roll(1)
 
-            rollAStrike(game) // -> 24
-            rollAStrike(game) // -> 15
-            game.roll(4)
-            game.roll(1)
+        game.score() shouldBe 44
+    }
 
-            game.score() shouldBe 44
-        }
+    @Test
+    fun `should be able to tell the score after three consecutive strikes`() {
+        val game = Game()
 
-        "should be able to tell the score after three consecutive strikes" {
-            val game = Game()
+        rollAStrike(game) // -> 30
+        rollAStrike(game) // -> 24
+        rollAStrike(game) // -> 15
+        game.roll(4)
+        game.roll(1)
 
-            rollAStrike(game) // -> 30
-            rollAStrike(game) // -> 24
-            rollAStrike(game) // -> 15
-            game.roll(4)
-            game.roll(1)
-
-            game.score() shouldBe 74
-        }
+        game.score() shouldBe 74
 
     }
 
