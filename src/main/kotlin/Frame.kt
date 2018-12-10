e13a2d2c4ab8d5d8d36046d875ff7fc74113471d
eureka84 <angelosciarra@ymail.com>
2018-12-10 22:46:16 +0100

    minor refactoring

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -5,6 +5,6 @@ typealias Frame = List<Int>
 
 fun Frame.isStrike(): Boolean = this.sum() == TOTAL_PINS && this.size == 1
 fun Frame.isSpare(): Boolean = this.sum() == TOTAL_PINS && this.size == MAX_THROWS_PER_FRAME
-fun Frame.firstThrow(): PinsKnockedDown = this[0]
+fun Frame.firstThrow(): PinsKnockedDown = this.head()
 fun Frame.pinsKnockedDown(): PinsKnockedDown = this.sum()
 fun emptyFrame(): Frame = emptyList()
