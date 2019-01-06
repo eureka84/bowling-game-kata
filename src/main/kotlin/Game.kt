7dd6030992486e76ca7602e96e56e9328e45c0a8
eureka84 <angelosciarra@ymail.com>
2019-01-06 16:07:20 +0100

    some refactoring

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -25,22 +25,28 @@ class Game {
     }
 
     private fun computeFrames(): List<Frame> {
-        val isFrameCompleted = { throws: Int, k: PinsKnockedDown -> throws == MAX_THROWS_PER_FRAME || k == TOTAL_PINS }
-        fun loop(rs: List<PinsKnockedDown>, f: Frame, fs: List<Frame>, throws: Int, k: PinsKnockedDown): List<Frame> {
-            return when {
-                rs.isEmpty() -> if (f.isEmpty()) fs else fs.plus<Frame>(f)
-                isFrameCompleted(throws, k) -> loop(rs, emptyFrame(), fs.plus<Frame>(f), 0, 0)
-                else -> loop(rs.tail(), f.plus(rs.head()), fs, throws + 1, k + rs.head())
-            }
+        val isFrameCompleted = { throws: Int, k: PinsKnockedDown ->
+            throws == MAX_THROWS_PER_FRAME || k == TOTAL_PINS
         }
+
+        fun loop(rs: List<PinsKnockedDown>, f: Frame, fs: List<Frame>, throws: Int, k: PinsKnockedDown): List<Frame> =
+                when {
+                    rs.isEmpty() ->
+                        if (f.isEmpty()) fs else fs.plus<Frame>(f)
+                    isFrameCompleted(throws, k) ->
+                        loop(rs, emptyFrame(), fs.plus<Frame>(f), 0, 0)
+                    else ->
+                        loop(rs.tail(), f.plus(rs.head()), fs, throws + 1, k + rs.head())
+                }
+
         return loop(rolls, emptyFrame(), listOf(), 0, 0)
     }
 
     private fun nextTwoThrowsPinsKnockedDown(frames: List<Frame>, frameNumber: Int) =
-        when {
-            frames[frameNumber + 1].isStrike() -> TOTAL_PINS + frames[frameNumber + 2].firstThrow()
-            else -> frames[frameNumber + 1].pinsKnockedDown()
-        }
+            when {
+                frames[frameNumber + 1].isStrike() -> TOTAL_PINS + frames[frameNumber + 2].firstThrow()
+                else -> frames[frameNumber + 1].pinsKnockedDown()
+            }
 
     private fun isBonusFrame(frameNumber: Int) = frameNumber > NUMBER_OF_FRAMES - 1
 
