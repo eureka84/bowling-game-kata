5f62ce468ca61088d97b3e39bce0867781a8c1f6
eureka84 <angelosciarra@ymail.com>
2019-01-07 08:25:56 +0100

    [WIP] creating sum type hierarchy for Frame

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -1,6 +1,41 @@
 package com.eureka.katas.bowling
 
 typealias PinsKnockedDown = Int
+sealed class KFrame {
+    abstract val firstThrow: PinsKnockedDown
+    internal abstract fun add(k: PinsKnockedDown): KFrame
+    companion object {
+        fun create(n: PinsKnockedDown): KFrame = when (n){
+            10 -> Strike
+            else -> NonStrike(firstThrow = n)
+        }
+    }
+}
+object Strike: KFrame() {
+    override fun add(k: PinsKnockedDown) = this
+    override val firstThrow: PinsKnockedDown = TOTAL_PINS
+}
+class NonStrike(override val firstThrow: PinsKnockedDown, private val secondThrow: PinsKnockedDown = 0): KFrame() {
+    override fun add(k: PinsKnockedDown) = NonStrike(firstThrow = this.firstThrow, secondThrow = k)
+}
+
+fun List<PinsKnockedDown>.toFrames(): List<KFrame> {
+
+    fun loop(rs: List<PinsKnockedDown>, f: KFrame?, fs: List<KFrame>, throws: Int, k: PinsKnockedDown): List<KFrame> =
+        if (rs.isEmpty()){
+            if (f == null) fs else fs.plus(f)
+        } else {
+            val frame = if (f == null ) KFrame.create(k) else {f.add(k)}
+            when (frame) {
+                Strike -> loop(rs, null, fs.plus(frame), 0, 0)
+                NonStrike(firstThrow = k) -> loop(rs.tail(), frame, fs, throws + 1, rs.head())
+                else -> loop(rs, null, fs.plus(frame), 0, 0)
+            }
+        }
+
+    return loop(this, null, listOf(), 0, 0)
+}
+
 typealias Frame = List<PinsKnockedDown>
 
 fun Frame.isStrike(): Boolean = this.sum() == TOTAL_PINS && this.size == 1
