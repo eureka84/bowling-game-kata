656a6dd6f2cdeb25a6e39492710b5a19e9f3adc2
eureka84 <angelosciarra@ymail.com>
2019-01-08 08:01:41 +0100

    some refactoring

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -12,7 +12,7 @@ object Strike : Frame() {
 
 data class Spare(
         override val firstThrow: PinsKnockedDown,
-        private val secondThrow: PinsKnockedDown = 0
+        private val secondThrow: PinsKnockedDown
 ) : Frame() {
     override val pinsKnockedDown: PinsKnockedDown = TOTAL_PINS
 }
@@ -29,13 +29,8 @@ fun List<PinsKnockedDown>.toFrames(): List<Frame> {
         throws == MAX_THROWS_PER_FRAME || k == TOTAL_PINS
     }
 
-    fun loop(
-            rs: List<PinsKnockedDown>,
-            f: PseudoFrame,
-            fs: List<PseudoFrame>,
-            throws: Int,
-            k: PinsKnockedDown
-    ): List<PseudoFrame> =
+    fun loop(rs: List<PinsKnockedDown>, f: PseudoFrame, fs: List<PseudoFrame>, throws: Int, k: PinsKnockedDown)
+            : List<PseudoFrame> =
             when {
                 rs.isEmpty() ->
                     if (f.isEmpty()) fs else fs.plus<PseudoFrame>(f)
@@ -48,7 +43,7 @@ fun List<PinsKnockedDown>.toFrames(): List<Frame> {
     return loop(this, emptyFrame(), listOf(), 0, 0).map { pseudoFrame ->
         when {
             pseudoFrame.size == 1 && pseudoFrame.sum() == TOTAL_PINS -> Strike
-            pseudoFrame.size == 1 -> Simple(pseudoFrame[0]) // used for running calculations
+            pseudoFrame.size == 1 -> Simple(pseudoFrame[0]) // used for incomplete games
             pseudoFrame.sum() == TOTAL_PINS -> Spare(pseudoFrame[0], pseudoFrame[1])
             else -> Simple(pseudoFrame[0], pseudoFrame[1])
         }
