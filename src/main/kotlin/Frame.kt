64b7c0903f2914c96f8341db856cb8367ed23fdd
eureka84 <angelosciarra@ymail.com>
2019-01-15 22:22:28 +0100

    little refactor

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -10,25 +10,21 @@ object Strike : Frame() {
     override val pinsKnockedDown: PinsKnockedDown = TOTAL_PINS
 }
 
-data class Spare(
-        override val firstThrow: PinsKnockedDown,
-        private val secondThrow: PinsKnockedDown
-) : Frame() {
+data class Spare(override val firstThrow: PinsKnockedDown, private val secondThrow: PinsKnockedDown) : Frame() {
     init {
-        require(firstThrow + secondThrow == TOTAL_PINS)
-        { "Spare pinsKnockerDown should equal $TOTAL_PINS" }
+        require(firstThrow + secondThrow == TOTAL_PINS) {
+            "Spare pinsKnockerDown should equal $TOTAL_PINS"
+        }
     }
 
     override val pinsKnockedDown: PinsKnockedDown = TOTAL_PINS
 }
 
-data class Simple(
-        override val firstThrow: PinsKnockedDown,
-        private val secondThrow: PinsKnockedDown = 0
-) : Frame() {
+data class Simple(override val firstThrow: PinsKnockedDown, private val secondThrow: PinsKnockedDown = 0) : Frame() {
     init {
-        require(firstThrow + secondThrow < TOTAL_PINS)
-        { "If pinsKnockerDown equal $TOTAL_PINS with 2 throws than it's a Spare" }
+        require(firstThrow + secondThrow < TOTAL_PINS) {
+            "If pinsKnockerDown equal $TOTAL_PINS with 2 throws than it's a Spare"
+        }
     }
 
     override val pinsKnockedDown: PinsKnockedDown = firstThrow + secondThrow
@@ -39,21 +35,21 @@ fun List<PinsKnockedDown>.toFrames(): List<Frame> {
         throws == MAX_THROWS_PER_FRAME || k == TOTAL_PINS
     }
 
-    fun loop(rs: List<PinsKnockedDown>, f: PseudoFrame, fs: List<Frame>, throws: Int, k: PinsKnockedDown)
-            : List<Frame> =
+    fun loop(rs: List<PinsKnockedDown>, f: PseudoFrame, fs: List<Frame>, throws: Int, k: PinsKnockedDown): List<Frame> =
             when {
                 rs.isEmpty() ->
-                    if (f.isEmpty()) fs else fs.plus(f.toFrame())
+                    if (f.isEmpty()) fs else fs + f.toFrame()
                 isFrameCompleted(throws, k) ->
-                    loop(rs, emptyPseudoFrame(), fs.plus(f.toFrame()), 0, 0)
+                    loop(rs, emptyPseudoFrame(), fs + f.toFrame(), 0, 0)
                 else ->
-                    loop(rs.tail(), f.plus(rs.head()), fs, throws + 1, k + rs.head())
+                    loop(rs.tail(), f + rs.head(), fs, throws + 1, k + rs.head())
             }
 
     return loop(this, emptyPseudoFrame(), listOf(), 0, 0)
 }
 
 typealias PseudoFrame = List<PinsKnockedDown>
+
 fun PseudoFrame.toFrame(): Frame =
         when {
             this.size == 1 && this.sum() == TOTAL_PINS -> Strike
