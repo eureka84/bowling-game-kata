2e6a0720a4adee624926c133dc689b733238a4d7
Angelo Sciarra <angelosciarra@ymail.com>
2021-07-16 07:53:26 +0200

    minor

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -2,8 +2,8 @@ data class Frame(
     private val firstThrow: PinsKnockedDown? = null,
     private val secondThrow: PinsKnockedDown? = null
 ) {
-    val pinsKnockedDownOnFirstThrow: Int = firstThrow.or(0) 
-    val pinsKnockedDown: PinsKnockedDown = firstThrow.or(0) + secondThrow.or(0)
+    val pinsKnockedDownOnFirstThrow: Int get() = firstThrow.or(0)
+    val pinsKnockedDown: PinsKnockedDown get() = firstThrow.or(0) + secondThrow.or(0)
 
     operator fun plus(pinsKnockedDown: PinsKnockedDown): Frame =
         if (firstThrow == null) {
@@ -12,7 +12,7 @@ data class Frame(
             this.copy(secondThrow = pinsKnockedDown)
         }
 
-    fun isComplete(): Boolean = firstThrow == TOTAL_PINS || (firstThrow != null && secondThrow != null)
+    fun isComplete(): Boolean = isStrike() || (firstThrow != null && secondThrow != null)
 
     fun isStrike(): Boolean = firstThrow == TOTAL_PINS
 
